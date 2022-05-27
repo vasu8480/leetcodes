e0f8653a3208c8eae10e77f3592e4e981b6f4711
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-05-27 11:51:19 +0530

    Add files via upload

---
+++ python/container_with_water.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxarea = 0
+        l = 0
+        r = len(height) - 1
+
+        while (l < r):
+            maxarea = max(maxarea, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return maxarea
+
+
+s = Solution()
+ans = s.maxArea([2, 1, 3, 4])
+print(ans)
---
+++ python/mountain.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def validMountainArray(self, A: List[int]) -> bool:
+        if len(A) < 3:
+            return False
+
+        i = 1
+        while i < len(A) and A[i] > A[i - 1]:
+            i += 1
+
+        if i == 1 or i == len(A):
+            return False
+
+        while i < len(A) and A[i] < A[i - 1]:
+            i += 1
+
+        return i == len(A)
+
+
+s = Solution()
+ans = s.validMountainArray([1, 3, 5, 4, 2])
+print(ans)
