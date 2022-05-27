65489153b1b18c3e2f143775b9d17e892c64a013
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-05-27 11:09:38 +0530

    Create binary_val.java

---
+++ java/binary_val/binary_val.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.BigInteger;
+class binary_val {
+  public static void main(String args[]) {
+	  
+	  String a="1010";
+	  String b="1011";
+	  
+	  System.out.println(new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2));
+	  
+	  
+	  }
+  }
+  
