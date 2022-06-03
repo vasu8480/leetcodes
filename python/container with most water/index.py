a900998c9efa4618ecdc3f82f1c80925c9c59b5d
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-06-03 15:28:10 +0530

    Update index.py

--- python/container with most water/index.py
+++ python/container with most water/index.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-		maxarea = 0
-		l = 0
-		r = len(height)-1
-
-		while(l<r):
-			maxarea = max(maxarea, min(height[l],height[r])*(r-l))
-			if(height[l]<height[r]):
-				l+=1
-			else:
-				r-=1
-		return maxarea
+        maxarea = 0
+        l = 0
+        r = len(height)-1
+        while(l<r):
+            maxarea = max(maxarea, min(height[l],height[r])*(r-l))
+            if(height[l]<height[r]):
+                l+=1
+            else:
+                r-=1
+        return maxarea
