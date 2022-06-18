9b7652c5bbd00892e4ec6298a5bfbef9193b8cfe
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-06-18 11:03:07 +0530

    Update index.py

--- python/merge 2 sorted lists/index.py
+++ python/merge 2 sorted lists/index.py
@@ -1,30 +1,29 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-		cur = ListNode(0)
-		ans = cur
-		
-		while(l1 and l2):
-			if(l1.val>l2.val):
-				cur.next = l2
-				l2 = l2.next
-			else:
-				cur.next = l1
-				l1 = l1.next
-			cur = cur.next
-			
-		while(l1):
-			cur.next = l1
-			l1 = l1.next
-			cur = cur.next
-		
-		while(l2):
-			cur.next = l2
-			l2 = l2.next
-			cur = cur.next
-		return ans.next
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        cur = ListNode(0)
+        ans = cur
+
+        while list1 and list2:
+            if list1.val > list2.val:
+                cur.next = list2
+                list2 = list2.next
+            else:
+                cur.next = list1
+                list1 = list1.next
+            cur = cur.next
+
+        while (list1):
+            cur.next = list1
+            list1 = list1.next
+            cur = cur.next
+
+        while (list2):
+            cur.next = list2
+            list2 = list2.next
+            cur = cur.next
+        return ans.next
