1074a868894f621dfe4dbf05919a302ac500fc2d
Vasu Nurukurthi <Vasu8480@gmail.com>
2022-10-05 11:24:26 +0530

    Update reverse-integer.js

--- javascript/reverse-integer.js
+++ javascript/reverse-integer.js
@@ -1,1 +1,12 @@
+var reverse = function(x) {
+	const number = Math.abs(x).toString().split('').reverse().join('');
 
+	 if (number > Math.pow(2, 31) - 1) {
+			 return 0
+	 }
+
+	 return x < 0 ? -number : number;
+};
+let x=1234;
+result=reverse(x)
+console.log(result);
